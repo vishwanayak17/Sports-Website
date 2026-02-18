401c305f7025d1bfcac8aae16b993088538444da
vishwanayak17 <vishwan1708@gmail.com>
2026-02-18 16:24:18 +0530

    Fix Home.jsx after resolving merge conflicts

--- sports-app/src/Components/Home/Home.jsx
+++ sports-app/src/Components/Home/Home.jsx
@@ -2,43 +2,21 @@
 import Herosection from '../Herosection/Herosection';
 import Game from '../Game/Game';
 import WhyChooseUs from '../howitworks/howitworks';
-import Feature from "../Feature/Feature"
+import Feature from "../Feature/Feature";
 import Register from '../register/register';
-<<<<<<< HEAD
 import Footer from '../footer/footer';
-import Features from "../Feature/Feature"
-=======
-// import Footer from '../footer/footer';
 
->>>>>>> 0045eed090aedee2d9b96ef7652ecb9b7691f254
-
-
-
-function Home(){
-  return(
-
-<>
-
+function Home() {
+  return (
+    <>
       {/* <Navbar/> */}
       <Herosection/>
       <Game/>
-<<<<<<< HEAD
-      <Features/>
-=======
       <Feature/>
->>>>>>> 0045eed090aedee2d9b96ef7652ecb9b7691f254
       <WhyChooseUs/>
       <Register/>
       {/* <Footer/> */}
-     
-     
-      
-<<<<<<< HEAD
-
-    </div>
-=======
-</>
->>>>>>> 0045eed090aedee2d9b96ef7652ecb9b7691f254
+    </>
   )
 }
 
